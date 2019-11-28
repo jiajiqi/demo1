a7a3f44742380623f64d8ea932ba38b5ebb1bdc4
xian-xian <1099693113@qq.com>
2019-11-28 14:58:31 +0800

    好了

--- src/HomePage/homepage.js
+++ src/HomePage/homepage.js
@@ -1,55 +1,55 @@
 import React from 'react';
-import {Link} from'react-router-dom';
-import {Layout,Row, Col,Input,Carousel,Icon,Button} from 'antd';
+import { Link } from 'react-router-dom';
+import { Layout, Row, Col, Input, Carousel, Icon, Button } from 'antd';
 
-var HomePageCss= require('./homepage.css')
-export default class HomePage extends React.Component{
-    render(){
-        return(
+var HomePageCss = require('./homepage.css')
+export default class HomePage extends React.Component {
+    render() {
+        return (
             <div>
                 <div className={HomePageCss.headline}>
                     <Row type="flex" justify="space-around">
-                    <Col span={24}></Col>
+                        <Col span={24}></Col>
                     </Row>
                 </div>
 
                 <div className={HomePageCss.firstline}>
-                <Row type="flex" justify="space-around">
-                    <Col span={3}></Col>
-                    <Col span={2}>
-                        名称
+                    <Row type="flex" justify="space-around">
+                        <Col span={3}></Col>
+                        <Col span={2}>
+                            名称
                     </Col>
-                    <Col span={7}>
-                    <Input.Search placeholder="搜索"
-                     onSearch={value => console.log(value)} style={{ width: 250 }}/>
-                    </Col>
-                    <Col span={1}>
-                        <Link className={HomePageCss.f}>首页</Link>
+                        <Col span={6}>
+                            <Input.Search placeholder="搜索"
+                                onSearch={value => console.log(value)} style={{ width: 250 }} />
                         </Col>
-                    <Col span={2}>
-                        <Link className={HomePageCss.f}>我们的服务</Link>
-                    </Col>
-                    <Col span={2}>
-                        <Link className={HomePageCss.f}>新的动态</Link>
+                        <Col span={2.1}>
+                            <Link className={HomePageCss.f}>首页</Link>
                         </Col>
-                    <Col span={2}>
-                        <Link className={HomePageCss.f}>联系我们</Link>
+                        <Col span={1.9}>
+                            <Link className={HomePageCss.f}>我们的服务</Link>
+                        </Col>
+                        <Col span={1.5}>
+                            <Link className={HomePageCss.f}>新的动态</Link>
+                        </Col>
+                        <Col span={2}>
+                            <Link className={HomePageCss.f}>联系我们</Link>
+                        </Col>
+                        <Col span={1.2}>
+                    <Link className={HomePageCss.f}><Button type="primary" style={{fontSize:'8px'}}  ><p>登录</p></Button></Link>
                     </Col>
-                    <Col span={1}>
-                    <Link className={HomePageCss.f}><Button type="primary">登录</Button></Link>
-                    </Col>
-                    <Col span={1}>
+                    <Col span={0.2}>
                     <p>|</p>
                     </Col>
-                    <Col span={1}>
-                    <Link className={HomePageCss.f}><Button type="primary">注册</Button></Link>
-                    </Col>
-                </Row>
+                    <Col span={2.3}>
+                    <Link className={HomePageCss.f}><Button type="primary" style={{fontSize:'8px'}}><p>注册</p></Button></Link>
+                    </Col> 
+                    </Row>
                 </div>
                 <div className={HomePageCss.secondline}>
                     <Row>
-                    <Col span={24}>
-                    <Carousel autoplay className={HomePageCss.l}>
+                        <Col span={24}>
+                            <Carousel autoplay className={HomePageCss.l}>
                         <div>
                            <img src="/taylor.jpg" width={'100%'} height={380}></img>
                         </div>
@@ -62,43 +62,43 @@ export default class HomePage extends React.Component{
                         <div>
                             <h3>4</h3>
                         </div>
-                    </Carousel>
-                    </Col>
+                    </Carousel> 
+                        </Col>
                     </Row>
                 </div>
                 <div className={HomePageCss.lastline}>
                     <Row>
-                    <Col span={2}></Col>
-                    <Col span={5}>
-                    <div className={HomePageCss.phone}>
-                            <Icon type="home" style={{fontSize:'40px'}} className={HomePageCss.phone1}/>
-                            <p >首页</p>
-                        </div>
-                    </Col>
-                    <Col span={5}>
-                        <div className={HomePageCss.phone}>
-                            <Icon type="user" style={{fontSize:'40px'}} className={HomePageCss.phone1}/>
-                            <p >个人主页</p>
-                        </div>
-                    </Col>
-                    <Col span={5}>
-                    <div className={HomePageCss.phone}>
-                            <Icon type="sketch" style={{fontSize:'40px'}} className={HomePageCss.phone1}/>
-                            <p >会员中心</p>
-                        </div>
-                    </Col>
-                    <Col span={5}>
-                        <div className={HomePageCss.phone}>
-                            <Icon type="phone" style={{fontSize:'40px'}} className={HomePageCss.phone1}/>
-                            <p >在线客服</p>
-                        </div>
-                    </Col>
-                    <Col span={2}></Col>
+                        <Col span={2}></Col>
+                        <Col span={5}>
+                            <div className={HomePageCss.phone}>
+                                <Icon type="home" style={{ fontSize: '40px' }} className={HomePageCss.phone1} />
+                                <p >首页</p>
+                            </div>
+                        </Col>
+                        <Col span={5}>
+                            <div className={HomePageCss.phone}>
+                                <Icon type="user" style={{ fontSize: '40px' }} className={HomePageCss.phone1} />
+                                <p >个人主页</p>
+                            </div>
+                        </Col>
+                        <Col span={5}>
+                            <div className={HomePageCss.phone}>
+                                <Icon type="sketch" style={{ fontSize: '40px' }} className={HomePageCss.phone1} />
+                                <p >会员中心</p>
+                            </div>
+                        </Col>
+                        <Col span={5}>
+                            <div className={HomePageCss.phone}>
+                                <Icon type="phone" style={{ fontSize: '40px' }} className={HomePageCss.phone1} />
+                                <p >在线客服</p>
+                            </div>
+                        </Col>
+                        <Col span={2}></Col>
                     </Row>
                 </div>
                 <div className={HomePageCss.addline}>
                     <Row>
-                    <Col span={24}>附加</Col>
+                        <Col span={24}>附加</Col>
                     </Row>
                 </div>
             </div>
